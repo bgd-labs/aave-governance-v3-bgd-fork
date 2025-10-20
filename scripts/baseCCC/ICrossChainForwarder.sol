2b18e7cb9462e805fa8512931491be8daeec17d1
sendra <polsendra@gmail.com>
2025-10-20 13:04:56 +0200

    fix: remove not needed adi lib

---
+++ scripts/baseCCC/ICrossChainForwarder.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+/**
+ * @title ICrossChainForwarder
+ * @author BGD Labs
+ * @notice interface containing the objects, events and methods definitions of the CrossChainForwarder contract
+ */
+interface ICrossChainForwarder {
+  /**
+   * @notice method to remove sender addresses
+   * @param senders list of addresses to remove
+   */
+  function removeSenders(address[] memory senders) external;
+
+  /**
+   * @notice method to approve new sender addresses
+   * @param senders list of addresses to approve
+   */
+  function approveSenders(address[] memory senders) external;
+}
